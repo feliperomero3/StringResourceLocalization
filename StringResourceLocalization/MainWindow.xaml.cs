c08dbeccd8cbafc8c2ac3c237e719cfb6e520102
Felipe Romero <feliperomero3@outlook.com>
2016-03-07 17:24:48 -0700

    Cambio de lenguaje al vuelo.

--- StringResourceLocalization/MainWindow.xaml.cs
+++ StringResourceLocalization/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
 using System.Threading;
 using System.Windows;
 
@@ -11,8 +13,57 @@ namespace StringResourceLocalization
     {
         public MainWindow()
         {
-            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("fr-FR");
             InitializeComponent();
+        }
+
+        private void menuItemEn_Click(object sender, RoutedEventArgs e)
+        {
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("en-US");
+            ResetMainWindow();
+
+            Debug.Print("en-US");
+        }
+
+        private void menuItemEs_Click(object sender, RoutedEventArgs e)
+        {
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("es-ES");
+            ResetMainWindow();
+
+            Debug.Print("es-ES");
+        }
+
+        private void menuItemFr_Click(object sender, RoutedEventArgs e)
+        {            
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("fr-FR");
+            ResetMainWindow();
+
+            Debug.Print("fr-FR");
+        }
+
+        private void menuItemDe_Click(object sender, RoutedEventArgs e)
+        {
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("de-DE");
+            ResetMainWindow();
+
+            Debug.Print("de-DE");
+        }
+
+        private void menuItemPr_Click(object sender, RoutedEventArgs e)
+        {
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("pt-BR");
+            ResetMainWindow();
+
+            Debug.Print("pt-BR");
+        }
+
+        private void ResetMainWindow()
+        {
+            Window oldWindow = Application.Current.MainWindow;
+
+            Application.Current.MainWindow = new MainWindow();
+            Application.Current.MainWindow.Show();
+
+            oldWindow.Close();
         }
     }
 }
